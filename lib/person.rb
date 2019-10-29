7cc5eea4d473e1fd66327333397680c38f2f8de0
github username <github email address>
2019-10-29 12:53:24 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,16 @@
 class Person
+
+def talk 
+    "Hello world!"
+end  
+
+def walk
+    puts "The Person is walking"
+end 
+
 end
 
 adele_goldberg = Person.new
 alan_kay = Person.new 
+
+alan_kay.walk 
